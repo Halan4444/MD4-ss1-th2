208c64496fc8d10bafd77a6a48ebbe2c718a6515
Trần Hạ Lan <halan4444b@gmail.com>
2022-05-27 11:55:29 +0700

    long2

---
+++ src/main/java/cg/wbd/grandemonstration/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package cg.wbd.grandemonstration.controller;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.CustomerServiceFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CustomerController {
+    private CustomerService customerService = CustomerServiceFactory.getInstance();
+
+    @GetMapping("/customers")
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("customers/list");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("/info")
+    public ModelAndView showInfor(@RequestParam long id) {
+        ModelAndView modelAndView = new ModelAndView("customers/info");
+        Customer customer = customerService.findOne(id);
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+}
